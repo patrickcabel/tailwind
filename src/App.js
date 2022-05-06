ece1f2d76315bb1f7b6627cd16b1dd40e6b97570
admin <admin@admins-MacBook-Air.local>
2022-05-06 14:38:17 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+import Login from './components/Login/Login';
+
+import useToken from "./useToken";
+
+
+
 
 function App() {
+
+  const {token, setToken} = useToken();
+
+  if(!token) {
+
+    return(
+    <BrowserRouter>
+    <Routes>
+
+  <Route 
+  path="/login" 
+  element={<Login setToken={setToken}
+  />} />
+
+    
+  </Routes>
+  </BrowserRouter>
+  );
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}>
+        </Route>
+        <Route path="/preferences" element={  <Preferences />}>
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import  NavBar from "../NavBar/NavBar";
+
+
+export default function Dashboard() {
+
+  return(
+    
+    <div>  <NavBar/>
+      <div className="grid grid-flow-col grid-cols-4 gap-4 ">
+ 
+ <section className="text-white hover:bg-blue-100">
+      
+     <article className="bg-black  border-2 rounded-md h-[400px] m-[10px]">
+        <p>Projects</p>
+     </article>
+
+<article className="bg-black border-2 rounded-md h-[400px] m-[10px]">
+        <p>Finances</p>
+ </article>
+
+<article class="bg-black border-2 rounded-md h-[400px] m-[10px]">
+       <p>HR</p>
+ </article>
+
+<article class="bg-black border-2 rounded-md h-[400px] m-[10px]">
+        <p>Team</p>
+ </article>
+
+</section>
+
+
+    </div>
+    </div>
+
+  );
+  
+}
