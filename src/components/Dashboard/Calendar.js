cda2422ccac64bca2cd647b0308b6d6d8001a8de
Tapiwa Kundishora <tapiwakundi@gmail.com>
2022-05-07 14:52:49 -0600

    add live timer

--- src/components/Dashboard/Calendar.js
+++ src/components/Dashboard/Calendar.js
@@ -3,26 +3,21 @@ import Moment from "react-moment";
 import moment from "moment";
 
 export default function Calendar() {
+    const [time, setTime] = React.useState(new Date().toLocaleString());
+
+    React.useEffect(() => {
+        let secTimer = setInterval(() => {
+            setTime(new Date().toLocaleString())
+        }, 1000)
+
+        return () => clearInterval(secTimer);
+    }, []);
+    
   return (
     <div class=" bg-[#6E30E9] justify-items-center rounded-full h-[350px] border-2  w-[350px] shadow-black shadow-xl static">
-      <div class=" flex flex-col items-center text-black font-extrabold">
-        <span class="text-color text-[#fff] text-[3em] text-extrabold leading-[1em] mt-[20px]">
-          <Moment format="MMMM" trim durationFromNowinterval={1000}></Moment>
+        <span>
+        {time}
         </span>
-        <span class="text-color text-[#fff] text-[3em] text-extrabold leading-[1.2em] mt-[20px] ">
-          <Moment format="Do" trim durationFromNowinterval={1000}></Moment>
-        </span>
-        <span class="text-color text-[#fff] text-[3em] text-extrabold leading-[2em] ">
-          <Moment
-            format="hh:mm:ss"
-            trim
-            durationFromNowinterval={1000}
-          ></Moment>
-        </span>
-        <span class="text-color text-[#fff] text-[3em] text-extrabold leading-[1.2em] mb-[20px]">
-          <Moment format="YYYY" trim durationFromNowinterval={1000}></Moment>
-        </span>
-      </div>
     </div>
   );
 }
