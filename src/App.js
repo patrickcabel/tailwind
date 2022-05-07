b359b544e2c613db906122b072d239e608c5990f
Tapiwa Kundishora <tapiwakundi@gmail.com>
2022-05-07 09:37:44 -0600

    fix app.js

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
 
 import useToken from "./useToken";
-
 function App() {
 
-  const AuthLayout = () => {
-    const { token, setToken } = useToken();
-    const location = useLocation();
-    return token
-      ? <Outlet />
-      : <Navigate to="/login" replace state={{ from: location }} />
-  }
+  const { token, setToken } = useToken();
 
-  const AnonymousLayout = () => {
+  if (!token) {
 
-    const { token } = useToken();
-    return token
-
-      ? <Navigate to="/" replace />
-      : <Outlet />
+    return (
+      <Router>
+        <Routes>
+          <Route path="/login">
+            <Route
+              index
+              element={<Login setToken={setToken}
+              />}
+            />
+          </Route>
+        </Routes>
+      </Router>
+    );
   }
 
   return (
-    <Router>
-      <Routes>
-        <Route element={<AnonymousLayout />}>
-          <Route path="/login" element={<Login setToken={useToken} />} />
-        </Route>
-        <Route element={<AuthLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preferences" element={<Preferences />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+          <Route path="/preferences" element={<Preferences />}>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
